febb0554b78b66e3a507718a960c125ee44ff002
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-08-15 15:51:05 +0100

    update css to include information updating

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,9 +1,13 @@
 function swapNav() {
     const navBar = document.querySelector(".sidebar");
+    const main = document.querySelector("main");
     if (navBar.style.width === "250px") {
         navBar.style.width = "25px";
+        main.style.marginLeft = "30px";
+
     } else {
         navBar.style.width = "250px";
+        main.style.marginLeft= "250px";
     }
 }
 
