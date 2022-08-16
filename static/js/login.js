3e30ead4584059a120c0297a55bb6d031e649667
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-08-16 11:41:25 +0100

    add fetch request for login

--- static/js/login.js
+++ static/js/login.js
@@ -18,3 +18,52 @@ document.querySelector(".popup-reg .close-btn").addEventListener("click", functi
     document.querySelector(".popup-reg").classList.remove("active");
 })
 
+//handing login function
+function login (data) {
+    
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    fetch("https://habit-harbour.herokuapp.com/login", options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => alert ("Login failed!"))
+}
+
+function register (data) {
+    console.log("register", data)
+}
+
+document.querySelector("#loginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    login({
+        username: form.get("username"),
+        password: form.get("password")
+    })
+
+    e.target.reset();
+})
+
+document.querySelector("#registerForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    register({
+        username: form.get("username2"),
+        password: form.get("password2")
+    })
+
+    e.target.reset();
+})
+
+
